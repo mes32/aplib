53a28b2a46cc405d0082ad9b62750d68c6a59e0f
mes32 <mes32@users.noreply.github.com>
2017-01-20 17:36:26 -0500

    feature: Add class 'APNumber'

---
+++ src/main/java/com/github/mes32/aplib/APNumber.java
@@ -0,0 +1,26 @@
+/*
+
+    APNumber - aplib
+
+ */
+
+package com.github.mes32.aplib;
+
+class APNumber {
+    //int base; Just assume base 10 for now
+    //NumericPrecision precision;
+    int[] digits;
+    int firstFractionalIndex;
+
+    APNumber(String strValue) {
+
+    }
+
+    APNumber(String strValue, int precision) {
+
+    }
+
+    String toString() {
+        return "0";
+    }
+}
