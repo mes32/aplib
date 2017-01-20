8f7adb5ade228ef621dc13d4ca8234343e48508c
mes32 <mes32@users.noreply.github.com>
2017-01-20 17:07:03 -0500

    feature: Add main class

---
+++ src/main/java/com/github/mes32/aplib/APLibMain.java
@@ -0,0 +1,15 @@
+/*
+
+    Main program for project aplib
+
+ */
+
+package com.github.mes32.aplib;
+
+import java.io.*;
+
+class APLibMain {
+    public static void main(String[] args) {
+        System.out.println("In class APLibMain...");
+    }
+}
